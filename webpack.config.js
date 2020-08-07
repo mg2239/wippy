c8e210daeb47e1ec17995bbd3b08e2d78d3fe59e
Matthew Guo <mguo22@gmail.com>
2020-08-06 22:39:13 -0400

    Fixed react router when deploying (#5)
    * potential fix
    
    * new fix
    
    * removed hot loader
    
    * removed head component
    
    * switched browserrouter to hashrouter
    
    * added vercel.json
    
    * removed clean and deploy from scripts

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = (env, argv) => ({
-  entry: ['react-hot-loader/patch', './src/index.tsx'],
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    filename: argv.hot ? '[name].[hash].js' : '[name].[contenthash].js',
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -59,7 +60,6 @@ module.exports = (env, argv) => ({
   resolve: {
     extensions: ['.js', '.jsx', '.tsx', '.ts'],
     alias: {
-      'react-dom': '@hot-loader/react-dom',
       src: path.resolve(__dirname, 'src'),
     },
   },

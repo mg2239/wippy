0a5395f6695c4e55f72050bf3f2b013ebd5550d1
Matthew Guo <mguo22@gmail.com>
2022-12-31 00:59:58 -0500

    maybe fix ios audio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import type { AppProps } from 'next/app';
+import unmuteAudio from 'unmute-ios-audio';
 import { UploadProvider } from '../context/UploadContext';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  unmuteAudio();
+
   return (
     <UploadProvider>
       <Component {...pageProps} />

088178a91974792609c04fce20e38580a5c60c9c
Matthew Guo <mguo22@gmail.com>
2022-12-31 01:00:24 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import unmuteAudio from 'unmute-ios-audio';
 import { UploadProvider } from '../context/UploadContext';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  unmuteAudio();
+  useEffect(() => {
+    unmuteAudio();
+  }, []);
 
   return (
     <UploadProvider>

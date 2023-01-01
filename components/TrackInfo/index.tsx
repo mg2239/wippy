32d7aba024014c932976d402227bec4da62bf427
Matthew Guo <mguo22@gmail.com>
2022-12-31 19:43:20 -0500

    404 title, title wrape

--- components/TrackInfo/index.tsx
+++ components/TrackInfo/index.tsx
@@ -11,7 +11,9 @@ export const TrackInfo = ({ track }: Props) => {
 
   return (
     <div className="mb-8">
-      <h1 className="mb-2 text-4xl font-semibold">{track.title}</h1>
+      <h1 className="mb-2 break-words text-3xl font-semibold sm:text-4xl">
+        {track.title}
+      </h1>
       <p>expires {expiration}</p>
     </div>
   );

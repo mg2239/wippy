88501472482b750a61bc61c303112d0132ced022
Matthew Guo <mguo22@gmail.com>
2022-12-31 22:34:16 -0500

    remove unmuteAudio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
-import unmuteAudio from 'unmute-ios-audio';
 import { UploadProvider } from '../context/UploadContext';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    unmuteAudio();
-  }, []);
-
   return (
     <UploadProvider>
       <Component {...pageProps} />
